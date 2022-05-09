0718cd11bff422dd6ee9c414ba44e14999e8b93f
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2022-05-09 16:42:00 -0300

    removed most placeholders ans starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and reload.
-        </p>
-        <p>
-          Hi my name is jhonny
-        </p>
-        <a
-          className="my-github"
-          href="https://github.com/newlog16"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Title />
       </header>
+      <body>
+        <h2>
+          
+        </h2>
+      </body>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+function Title() {
+    return (
+        <h1>
+            <p>
+                Hi my name is Jhonny
+            </p>
+        </h1>
+
+    );
+}
+
+export default Title;
